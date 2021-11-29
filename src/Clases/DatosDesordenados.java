2cabb1eaf3ea11ba1b2f5f88d8d264a8501a03de
YadiraL2 <94420279+YadiraL2@users.noreply.github.com>
2021-11-28 19:47:56 -0600

    Update DatosDesordenados.java

--- src/Clases/DatosDesordenados.java
+++ src/Clases/DatosDesordenados.java
@@ -60,6 +60,47 @@ public class DatosDesordenados {
         System.out.println("BS-intercambios="+ci);
         return new DatosOrdenados(arreglo);
     }
+    
+    static int mo=0;
+    public static void quicksort(int arreglo[], int izq, int der) {
+        int pivote=arreglo[izq]; // tomamos primer elemento como pivote
+        int i=izq;         // i realiza la búsqueda de izquierda a derecha
+        int j=der;         // j realiza la búsqueda de derecha a izquierda
+        int aux;
+ 
+        while(i < j){                          // mientras no se crucen las búsquedas                                   
+            while(arreglo[i] <= pivote && i < j) 
+                i++; // busca elemento mayor que pivote
+            while(arreglo[j] > pivote) 
+                j--;           // busca elemento menor que pivote
+            if (i < j) {                        // si no se han cruzado                      
+                aux= arreglo[i];                      // los intercambia
+                arreglo[i]=arreglo[j];
+                arreglo[j]=aux;
+            }
+            mo++;
+        }
+   
+        arreglo[izq]=arreglo[j];      // se coloca el pivote en su lugar de forma que tendremos                                    
+        arreglo[j]=pivote;      // los menores a su izquierda y los mayores a su derecha
+   
+        if(izq < j-1) {
+            quicksort(arreglo,izq,j-1);          // ordenamos subarray izquierdo
+            mo++;
+        }
+        if(j+1 < der){
+            quicksort(arreglo,j+1,der);          // ordenamos subarray derecho
+            mo++;
+        }
+    }
+    
+    public DatosOrdenados QuickSort(){
+        int [] q = this.getCopiaValores();
+	    this.quicksort(q, 0, q.length-1);
+        System.out.println("QS-comparaciones = " + mo);
+	    return new DatosOrdenados(q);
+    }
+    
     @Override
     public String toString(){
         String cad="Datos desordenados";
