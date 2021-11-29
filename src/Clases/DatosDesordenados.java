72a7496b8b7c7074d56b590563418fe311a61c22
Adrian119Tec <94420583+Adrian119Tec@users.noreply.github.com>
2021-11-28 20:35:12 -0600

    Update DatosDesordenados.java

--- src/Clases/DatosDesordenados.java
+++ src/Clases/DatosDesordenados.java
@@ -100,7 +100,53 @@ public class DatosDesordenados {
         System.out.println("QS-comparaciones = " + mo);
 	    return new DatosOrdenados(q);
     }
-    
+    public static void radixSort(int[] arr) {
+        int digit = getMaxDigit (arr); // obtiene el número máximo de dígitos
+        for (int i = 0; i < digit; i++) {
+             
+                         bucketSort (arr, i); // ordena bucketSort por dígitos veces
+                         
+        }
+    }
+ 
+    public static int getMaxDigit(int[] arr) {
+                 int digit = 1; // el valor predeterminado es solo un dígito
+                 int base = 10; // Cada dígito adicional en decimal significa que su valor es 10 veces mayor
+        for (int i : arr) {
+            while (i > base) {
+                digit++;
+                base *= 10;
+            }
+        }
+        return digit;
+    }
+ 
+    public static void bucketSort(int[] arr, int digit) {
+        int base = (int) Math.pow(10, digit);
+        // init buckets
+        ArrayList<ArrayList<Integer>> buckets = new ArrayList<ArrayList<Integer>>();
+                 for (int i = 0; i <10; i ++) {// Solo hay diez números del 0 al 9, así que prepara diez cubos
+            buckets.add(new ArrayList<Integer>());
+        }
+        // sort
+        for (int i : arr) {
+            int index = i / base % 10;
+            buckets.get(index).add(i);
+        }
+        // output: copy back to arr
+        int index = 0;
+        for (ArrayList<Integer> bucket : buckets) {
+            for (int i : bucket) {
+                arr[index++] = i;
+            }
+        }
+    }
+ 
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
     @Override
     public String toString(){
         String cad="Datos desordenados";
